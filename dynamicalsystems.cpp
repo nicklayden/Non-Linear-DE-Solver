8810bc95ded2d1a387241a92664fbd029a632c84
nick layden <nicholas.layden@gmail.com>
2017-10-14 19:31:12 -0300

    Added C++ field solver

---
+++ dynamicalsystems.cpp
@@ -0,0 +1,11 @@
+/**
+ * @Author: Nicholas Layden <nick>
+ * @Date:   2017-10-14T19:29:21-03:00
+ * @Email:  nicholas.layden@gmail.com
+ * @Filename: dynamicalsystems.cpp
+ * @Last modified by:   nick
+ * @Last modified time: 2017-10-14T19:29:21-03:00
+ */
+
+
+
